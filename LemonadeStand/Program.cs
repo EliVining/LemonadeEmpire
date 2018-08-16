7ec16adae1017582f299069416a4455c9198f671
Chip <chip.elmer@gmail.com>
2018-08-16 10:18:51 -0500

    Added some string interpolation

--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -17,7 +17,7 @@ namespace LemonadeStand
             {
                 LemonadeStand lemonadeStand = new LemonadeStand();
 
-                Console.WriteLine("What is the name of location " + (i+1) + "?");
+                Console.WriteLine($"What is the name of location {i+1}?");
                 lemonadeStand.Name = Console.ReadLine();
                              
                 Console.WriteLine("How many cups will you sell?");
@@ -25,17 +25,17 @@ namespace LemonadeStand
                
                 Console.WriteLine("How much will you sell each cup for?");
                 lemonadeStand.PricePerCup = decimal.Parse(Console.ReadLine());
-
                
                 Console.WriteLine("How much will each cup cost to make?");
                 lemonadeStand.CostPerCup = decimal.Parse(Console.ReadLine());
                
-                Console.WriteLine("You'll be opening a stand called " + lemonadeStand.Name + ".");
-                Console.WriteLine("If you sell all " + lemonadeStand.NumberOfCups + "cups");
-                Console.WriteLine("Your total revenue will be $" + lemonadeStand.GetTotalRevenue() + ".");
-                Console.WriteLine("Your total expnses will be $" + lemonadeStand.GetTotalExpenses() + ".");
-                Console.WriteLine("Your total profit will be $" + lemonadeStand.GetTotalProfit() + ".");             
+                Console.WriteLine($"You'll be opening a stand called {lemonadeStand.Name}.");
+                Console.WriteLine($"If you sell all {lemonadeStand.NumberOfCups} cups");
+                Console.WriteLine($"Your total revenue will be ${lemonadeStand.GetTotalRevenue()}.");
+                Console.WriteLine($"Your total expnses will be ${lemonadeStand.GetTotalExpenses()}.");
+                Console.WriteLine($"Your total profit will be ${lemonadeStand.GetTotalProfit()}.");             
             }
+
             Console.ReadLine();           
         }
     }
