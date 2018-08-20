15236ff64406a4b7bac97160723d019da9eea07c
Dimitrius Floyd <dimitriusfloyd@gmail.com>
2018-08-20 09:37:07 -0500

    added hotdog and popsicle class

---
+++ LemonadeStand/Hotdog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//created Hotdaog class so you can expand your empire
+//this class inherits from the lemonadestand class
+namespace LemonadeStand
+{
+    class Hotdog : LemonadeStand
+    {
+    }
+}
---
+++ LemonadeStand/Popsicle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//same as hotdog 
+namespace LemonadeStand
+{
+    class Popsicle : LemonadeStand
+    {
+    }
+}
